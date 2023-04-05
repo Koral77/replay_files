f20289849c788238c68b2ad437a1635a98ff556d
Zhangbin <58699405+Koral77@users.noreply.github.com>
2023-04-05 16:42:28 +0800

    Update main.rs

--- replays/unicode-segmentation/replay_unicode-segmentation2/src/main.rs
+++ replays/unicode-segmentation/replay_unicode-segmentation2/src/main.rs
@@ -2,5 +2,5 @@ extern crate unicode_segmentation;
 
 fn main() {
     let mut _local0 = unicode_segmentation::GraphemeCursor::new(8463800222054970741 ,8463951407229173877 ,false);
-    unicode_segmentation::GraphemeCursor::is_boundary(&mut (_local0), "Ë", 8463800222054970740);
+    let _ = unicode_segmentation::GraphemeCursor::is_boundary(&mut (_local0), "Ë", 8463800222054970740);
 }
