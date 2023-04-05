64dfda4105ed237bb6060cf96a0c105dea9218a1
Zhangbin <58699405+Koral77@users.noreply.github.com>
2023-04-05 16:41:49 +0800

    Update main.rs

--- replays/unicode-segmentation/replay_unicode-segmentation11/src/main.rs
+++ replays/unicode-segmentation/replay_unicode-segmentation11/src/main.rs
@@ -2,6 +2,5 @@ extern crate unicode_segmentation;
 
 fn main() {
     let mut _local0 = unicode_segmentation::GraphemeCursor::new(18446744073709551615 ,16212958658533785599 ,false);
-    unicode_segmentation::GraphemeCursor::next_boundary(&mut (_local0), "0", 18446744073709551615);
-    unicode_segmentation::GraphemeCursor::prev_boundary(&mut (_local0), ":0", 18388250262347198512);
+    let _ = unicode_segmentation::GraphemeCursor::next_boundary(&mut (_local0), "0", 18446744073709551615);
 }
