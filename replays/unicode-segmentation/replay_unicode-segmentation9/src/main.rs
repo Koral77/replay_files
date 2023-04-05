77671994a6506a9a44bb6b2a5e5170ca6797c46a
Zhangbin <58699405+Koral77@users.noreply.github.com>
2023-04-05 16:48:31 +0800

    Update main.rs

--- replays/unicode-segmentation/replay_unicode-segmentation9/src/main.rs
+++ replays/unicode-segmentation/replay_unicode-segmentation9/src/main.rs
@@ -2,6 +2,5 @@ extern crate unicode_segmentation;
 
 fn main() {
     let mut _local0 = unicode_segmentation::GraphemeCursor::new(5208492444341520456 ,5208492444341520431 ,true);
-    unicode_segmentation::GraphemeCursor::prev_boundary(&mut (_local0), "HHHHHHHHHjHHH", 5208492444341520456);
-    unicode_segmentation::GraphemeCursor::next_boundary(&mut (_local0), "HHHHHHHHHHHHH", 5208492589950978632);
+    let _ = unicode_segmentation::GraphemeCursor::next_boundary(&mut (_local0), "HHHHHHHHHHHHH", 5208492589950978632);
 }
