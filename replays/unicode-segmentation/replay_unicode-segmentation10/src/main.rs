81f4bd17b6f8cd52fcc56637c08617cc26468dc7
Zhangbin <58699405+Koral77@users.noreply.github.com>
2023-04-05 16:41:06 +0800

    Update main.rs

--- replays/unicode-segmentation/replay_unicode-segmentation10/src/main.rs
+++ replays/unicode-segmentation/replay_unicode-segmentation10/src/main.rs
@@ -2,6 +2,5 @@ extern crate unicode_segmentation;
 
 fn main() {
     let mut _local0 = unicode_segmentation::GraphemeCursor::new(5208492444341520467 ,3407250190757808200 ,true);
-    unicode_segmentation::GraphemeCursor::prev_boundary(&mut (_local0), "HHHZ\\HHH\0\u{e040}HHK", 5208492444341520456);
-    unicode_segmentation::GraphemeCursor::next_boundary(&mut (_local0), "\0\"H[HH(HHH?HH\u{e040}", 5192729847860321864);
+    let _ = unicode_segmentation::GraphemeCursor::prev_boundary(&mut (_local0), "HHHZ\\HHH\0\u{e040}HHK", 5208492444341520456);
 }
