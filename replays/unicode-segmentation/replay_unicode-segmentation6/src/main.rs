70b7fe88e6d3ead6c1a9c0ab21028d468f87b4b5
Zhangbin <58699405+Koral77@users.noreply.github.com>
2023-04-05 16:45:17 +0800

    Update main.rs

--- replays/unicode-segmentation/replay_unicode-segmentation6/src/main.rs
+++ replays/unicode-segmentation/replay_unicode-segmentation6/src/main.rs
@@ -2,6 +2,5 @@ extern crate unicode_segmentation;
 
 fn main() {
     let mut _local0 = unicode_segmentation::GraphemeCursor::new(5404402016221612875 ,5425481077020773195 ,false);
-    unicode_segmentation::GraphemeCursor::prev_boundary(&mut (_local0), "KKK", 5425512962414627659);
-    unicode_segmentation::GraphemeCursor::next_boundary(&mut (_local0), "KKK1", 5425512838301707595);
+    let _ = unicode_segmentation::GraphemeCursor::prev_boundary(&mut (_local0), "KKK", 5425512962414627659);
 }
